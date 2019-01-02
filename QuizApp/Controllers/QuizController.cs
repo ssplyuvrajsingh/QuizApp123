e36b7e383ef368e83b598ecc78c815cf962d4f54
sumedha11 <sumedha11@SUMEDHA11-PC>
2019-01-02 12:17:17 +0530

    changes

--- QuizApp/Controllers/QuizController.cs
+++ QuizApp/Controllers/QuizController.cs
@@ -6,12 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 
 namespace QuizApp.Controllers
 {
     [Authorize]
-    public class QuizController : Controller
+    public class QuizController : ApiController
     {
         #region Get Quiz
 
