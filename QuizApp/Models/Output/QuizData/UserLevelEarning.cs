e1ceca06b7ecc4e98fe69289afcd7df4e9e491c1
shyam <shyam@sumedhasoftech.com>
2020-01-10 10:47:39 +0530

    Update UserLevelEarning.cs

--- QuizApp/Models/Output/QuizData/UserLevelEarning.cs
+++ QuizApp/Models/Output/QuizData/UserLevelEarning.cs
@@ -15,7 +15,6 @@ namespace QuizApp.Models.Output.QuizData
         public Nullable<System.DateTime> LastActiveDate { get; set; }
         public string ReferalCode { get; set; }
         public string UsedReferalCode { get; set; }
-
         public Nullable<bool> isActive { get; set; }
     }
 
