64c96ed4a33163924ce9bf0cf1f2f383bc0adfe1
shyam <shyam@sumedhasoftech.com>
2020-02-20 17:56:02 +0530

    Update User.cs

--- QuizAdmin/Models/User.cs
+++ QuizAdmin/Models/User.cs
@@ -42,6 +42,8 @@ namespace QuizAdmin.Models
         public string Bank { get; set; }
         public string IFSCCode { get; set; }
         public string Passcode { get; set; }
+        public string googleAdsId { get; set; }
+        public string facebookAdsId { get; set; }
     
         public virtual AspNetUser AspNetUser { get; set; }
     }
